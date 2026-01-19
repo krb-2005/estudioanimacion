e99a83247b9b0f4b9018db3f3d23d9b5a62d1c8a
krb-2005 <ruben.penlaz@educa.jcyl.es>
2026-01-19 10:01:38 +0100

    Cambio en el modelo storyboard; ya no hay campos computados

--- models/storyboard.py
+++ models/storyboard.py
@@ -6,12 +6,8 @@ class storyboard(models.Model):
     _name = 'estudioanimacion.storyboard'
     _description = 'estudioanimacion.storyboard'
     
-    codigo = fields.Char(compute="get_code", string="Código del storyboard", readonly=True, required=True)
+    codigo = fields.Char(string="Código del storyboard", required=True)
     descripcion = fields.Char(string="Descripción", required=True)
     
     supervisor_id = fields.Many2one("estudioanimacion.supervisor", string="Supervisor")
     proyecto_id = fields.Many2one("estudioanimacion.proyecto", string="Proyecto", ondelete="cascade")
-    
-    def get_code(self):
-        for storyboard in self:
-            storyboard.codigo = "Escena " + str(storyboard.id)
